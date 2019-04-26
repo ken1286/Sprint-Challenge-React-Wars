779c9fa6a256017eb4f80baf8b5bb6dcb221207f
ken1286 <kindle1286@gmail.com>
2019-04-26 09:40:11 -0700

    styling finished

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,7 @@ import './StarWars.css';
 
 function Character(props) {
   return(
-    <div className="char-traits">
+    <div className="char-card">
       <h1>Name: {props.charName}</h1>
       <h3>Gender: {props.gender}</h3>
       <h3>Birth Year: {props.birthYear}</h3>
